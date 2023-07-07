522454927cb3bc80f1c7f53aa21e6345c24d769d
nidhimahajan123456 <mahajannidhi388@gmail.com>
2023-07-07 14:18:00 +0530

    second commit

--- client/src/components/input/index.js
+++ client/src/components/input/index.js
@@ -14,7 +14,7 @@ const index = ({
     return (
         <div className='w-1/2'>
          {/* <div className={`${className}`}> */}
-            <label for={name} className="block text-sm font-medium text-gray-800">{label}</label>
+            <label htmlFor={name} className="m-2 block text-sm font-medium text-gray-800">{label}</label>
             <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${inputClassName}`} placeholder={placeholder} required={isRequired} value={value} onChange={onChange} />
         </div>
     )
